1fa1e0857051c758811eaffc02c5defbf7851222
depth01 <doninura95@gmail.com>
2022-10-29 14:46:50 +0200

    Added Column and three elevated buttons

--- lib/main.dart
+++ lib/main.dart
@@ -7,12 +7,24 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    var questions = [
+      'What is tour fav color?',
+      'Favorite Animal?',
+    ];
+
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: Text("Quiz App"),
         ),
-        body: Text("Are you here?"),
+        body: Column(
+          children: <Widget>[
+            Text("The question"),
+            ElevatedButton(onPressed: () => {}, child: Text("Answer1")),
+            ElevatedButton(onPressed: () => {}, child: Text("Answer2")),
+            ElevatedButton(onPressed: () => {}, child: Text("Answer3")),
+          ],
+        ),
       ),
     );
   }
