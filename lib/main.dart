84dd097cefa47283fab980e7acea3c4f3603da80
depth01 <doninura95@gmail.com>
2022-10-29 13:32:18 +0200

    texti u ndryshu pak

--- lib/main.dart
+++ lib/main.dart
@@ -7,7 +7,7 @@ void main() {
 class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Text('Hello Boss'),
+      home: Text('Hello Bossi Toni'),
     );
   }
 }
